8f1b0351d079c6241f86a0da169ef385c757b727
qkrwo <qkrwo@LAPTOP-Q6P4M18G>
2021-03-22 20:50:43 +0900

    2021-03-22

---
+++ src/Test/Test10039.java
@@ -0,0 +1,30 @@
+package Test;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Test10039 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int score;
+		int avg = 0;
+		
+		for(int i=0;i<5;i++) {
+			score = sc.nextInt();
+			
+			if(score <40) {
+				score=40;
+				avg += score;
+			}
+			else if (score>0 && score < 101) 
+				avg +=score;
+		}
+	
+		System.out.println(avg/5);
+		
+		
+
+	}
+
+}
---
+++ src/Test/Test5543.java
@@ -0,0 +1,32 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Test5543 {
+	public static void main(String[] args) {
+
+		int b;
+		int d;
+		Scanner sc = new Scanner(System.in);
+
+		b = 2001;
+		d = 2001;
+
+		for (int i = 0; i < 3; i++) {
+			int v = sc.nextInt();
+			if (v < b) {
+				b = v;
+			}
+
+		}
+		for (int i = 0; i < 2; i++) {
+			int vv = sc.nextInt();
+			if (vv < d) {
+				d = vv;
+			}
+
+		}
+		System.out.println(b + d - 50);
+
+	}
+}
