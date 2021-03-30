7727a3d9a6bd10fa719161425efcbbb547930e94
parkjaewon1 <48577510+parkjaewon1@users.noreply.github.com>
2021-03-30 23:39:36 +0900

    2021-03-30

---
+++ src/Test/Ex11653.java
@@ -0,0 +1,28 @@
+package Test;
+
+import java.util.*;
+
+public class Ex11653 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		if (N != 0 && N < 10000000) {
+
+			for (int i = 2; i < N; i++) {
+				while (N % i == 0) {
+					System.out.println(i);
+					N = N / i;
+				}
+			}
+			if(N!=1) {
+			System.out.println(N);
+			}
+		}
+
+	}
+
+}
