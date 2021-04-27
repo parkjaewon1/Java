5c3379f1589c4ccbdac26b4fae43241fec2e3f16
parkjaewon1 <48577510+parkjaewon1@users.noreply.github.com>
2021-04-28 00:15:49 +0900

    2021-04-28

--- src/Test_04_27/Ex1026.java
+++ src/Test_04_27/Ex1026.java
@@ -1,10 +1,30 @@
 package Test_04_27;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Ex1026 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		int[] B = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			A[i]= sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			B[i]= sc.nextInt();
+		}
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += A[i] * B[N-1-i];
+		}
 
+		System.out.println(sum);
 	}
 
 }
